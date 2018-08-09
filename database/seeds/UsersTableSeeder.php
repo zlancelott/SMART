0af5ebea9fdf083643368cf8ac1e63bf1bed1e2a
ronald_araujo <f.ronaldaraujo@gmail.com>
2018-08-09 00:22:57 -0300

    Teste

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,5 +16,17 @@ class UsersTableSeeder extends Seeder
             'email' => 'super@app.com',
             'profile' => config('profiles.superadmin')
         ]);
+
+        factory(App\Models\User::class)->create([
+            'name' => 'Admin',
+            'email' => 'admin@app.com',
+            'profile' => config('profiles.admin')
+        ]);
+
+        factory(App\Models\User::class)->create([
+            'name' => 'Operador',
+            'email' => 'operador@app.com',
+            'profile' => config('profiles.operator')
+        ]);
     }
 }
